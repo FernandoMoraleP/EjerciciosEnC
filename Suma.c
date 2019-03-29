579661b6f0d3a2fd899287813da988ac8b2c4856
FerMoral <derox58@gmail.com>
2019-03-29 15:55:53 -0600

    suma

---
+++ Suma.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	int j;
+	int Res;
+
+	printf("Ingrese el primer numero\n");
+	scanf("%d", &i);
+	printf("Ingrese el segundo numero\n");
+	scanf("%d", &j);
+
+	Res = i + j;
+	printf("El resultado es: %d\n", Res);
+	/* code */
+	return 0;
+}
